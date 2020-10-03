934f95c1baaca79dbe1e724792bcc60d7fa99003
Jake Wagner <jkwagneriii@gmail.com>
2020-10-03 16:10:23 -0500

    this is post tutor session. tom and i worked on how to add my elements into the document through JS. We also fleshed out my main functions to make the text areas react to what time it is. going back in to work on comments.

--- Develop/psuedo.js
+++ Develop/psuedo.js
@@ -2,7 +2,6 @@
 
 //When I click onto one of the timeslots, AKA the form, I can type in some text that I would like to save to my schedule. That form will be connected to a button that saves the text submission to the local storage of my device. When the page is reloaded, my text will still be in the correct timeslot. 
 
-
 //Remember - must use Moment.js to work with date & time.
 // https://momentjs.com/
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.0/moment.min.js" integrity="sha512-Izh34nqeeR7/nwthfeE0SI3c8uhFSnqxV0sI9TvTcXiFJkMd6fB644O64BRq2P/LA/+7eRvCw4GmLsXksyTHBg==" crossorigin="anonymous"></script>
--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,87 @@ function displayTime () {
 }
 
 displayTime();
+
+function timeCheck(currentRow, currentTime) {
+    if (currentRow < currentTime) {
+        return 'past'
+    } else if (currentRow === currentTime) {
+        return 'present'
+    } else {
+        return 'future'
+    } 
+}
+
+function displayRows() {
+
+    console.log('moment stuff', moment().hour())
+    for (let i = 9; i < 18; i++) {
+
+        //1 create html element with jquery
+        var rowDiv = $('<div>')
+        var timeDiv = $('<div>')
+        var textArea = $('<textarea>')
+        var button = $('<button>')
+
+        //2 dress it up how we want
+        rowDiv.addClass('row')
+        timeDiv.addClass('col-md-2 time-block')
+        textArea.addClass(`col-md-8 ${timeCheck(i, moment().hour())}`)
+        timeDiv.text(i + ' AM')
+        button.attr('name', i)
+         textArea.attr('id', 'input-' + i)
+
+        if( i > 12 ) {
+
+            var displayHour = i - 12
+            timeDiv.text(displayHour + ' PM')
+            button.attr('name', displayHour)
+           
+            textArea.attr('id', 'input-' + displayHour)
+        } else if (i === 12) {
+            timeDiv.text(i + ' PM')
+            
+        }
+
+        button.addClass('col-md-2 saveBtn')
+        // .append to page!!
+        rowDiv.append(timeDiv, textArea, button)
+        $('.scheduler').append(rowDiv)
+    }
+}
+
+displayRows()
+
+$('.saveBtn').on('click', function() {
+    console.log('You got clicked', $(this).attr('name'));
+    console.log('what we typd', $('#input-' + $(this).attr('name')).val())
+
+    var dataToSave
+    var oldData = JSON.parse(localStorage.getItem('scheduler-data'))
+    
+    var startingData ={
+        9: '',
+        10: '',
+        11: '',
+        12: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: ' '
+    }
+
+    if (oldData) {
+        dataToSave = oldData
+    } else {
+        dataToSave = startingData
+    }   
+    dataToSave[$(this).attr('name')] = $('#input-' + $(this).attr('name')).val()
+
+    console.log('startind data /???',dataToSave)
+    var strData = JSON.stringify(dataToSave)
+
+    localStorage.setItem('scheduler-data', strData)
+})
+
+
